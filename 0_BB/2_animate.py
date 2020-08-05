d07b08d35f137607c5aa595ed943b4716a5daeb6
‍김수영(대학원학생/일반대학원 컴퓨터과학과) <naikiki87@naver.com>
2020-08-05 17:37:49 +0900

    BB2

--- 0_BB/2_animate.py
+++ 0_BB/2_animate.py
@@ -9,9 +9,6 @@ pause = False
 
 Gravity = 9.8
 
-# v0_km = random.randint(80, 140)
-# v0_m = v0_km * 1000 / 3600
-# angle = random.randint(1, 170)
 v0_km = random.randint(120, 140)
 v0_m = v0_km * 1000 / 3600
 angle = random.randint(35, 50)
@@ -24,26 +21,46 @@ cos = math.cos(radian)
 t_max = (2 * v0_m * sin) / Gravity
 
 def simData():
-    # t_max = 10.0
     dt = 0.1
     x = 0.0
     t = 0.0
-    while t < t_max:
-        if not pause:
-            x = v0_m * cos * t
-            y = v0_m * sin * t - 0.5 * Gravity * t * t
-            t = t + dt
-        yield x, y, t
+    while t <= (t_max + 0.1):
+        x = v0_m * cos * t
+        y = v0_m * sin * t - 0.5 * Gravity * t * t
+        t = t + dt
+        
+        if y >= 0 :
+            yield x, y, t
+        # if not pause:
+        #     x = v0_m * cos * t
+        #     y = v0_m * sin * t - 0.5 * Gravity * t * t
+        #     t = t + dt
+        # yield x, y, t
 
 def onClick(event):
-    global pause
-    pause ^= True
+    global v0_m, angle, sin, cos, t_max
+
+    v0_km = random.randint(120, 140)
+    v0_m = v0_km * 1000 / 3600
+    angle = random.randint(35, 50)
+    radian = math.radians(angle)
+    sin = math.sin(radian)
+    cos = math.cos(radian)
+    t_max = (2 * v0_m * sin) / Gravity
+
+    print('v0_m : ', round(v0_m, 1), ", angle : ", angle)
+
+    show_animation()
+    # global pause
+    # pause ^= True
 
 def simPoints(simData):
     x, y, t = simData[0], simData[1], simData[2]
     time_text.set_text(time_template%(t))
     v0_text.set_text(v0_template%(v0_m))
     angle_text.set_text(angle_template%(angle))
+    pos_x_text.set_text(pos_x_template%(x))
+    pos_y_text.set_text(pos_y_template%(y))
     line.set_data(x, y)
     return line, time_text
 
@@ -56,11 +73,20 @@ ax.set_xlim(-20, 130)
 time_template = 'Time = %.1f s'
 v0_template = 'v0 = %.1f m/s'
 angle_template = 'Angle = %.1f º'
+pos_x_template = 'X = %.1f m'
+pos_y_template = 'Y = %.1f m'
 
 v0_text = ax.text(0.1, 0.9, '', transform=ax.transAxes)
 angle_text = ax.text(0.1, 0.85, '', transform=ax.transAxes)
 time_text = ax.text(0.1, 0.8, '', transform=ax.transAxes)
 
-# fig.canvas.mpl_connect('button_press_event', onClick)
-ani = animation.FuncAnimation(fig, simPoints, simData, blit=False, interval=10, repeat=False)
-plt.show()
+pos_x_text = ax.text(0.5, 0.85, '', transform=ax.transAxes)
+pos_y_text = ax.text(0.5, 0.8, '', transform=ax.transAxes)
+
+fig.canvas.mpl_connect('button_press_event', onClick)
+
+def show_animation() :
+    ani = animation.FuncAnimation(fig, simPoints, simData, blit=False, interval=10, repeat=False)
+    plt.show()
+
+show_animation()
