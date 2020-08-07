457b464b02119e161604a581d703669c5eb1199d
‍김수영(대학원학생/일반대학원 컴퓨터과학과) <naikiki87@naver.com>
2020-08-07 14:41:08 +0900

    fp final

--- SOCKET/8_200806_total.py
+++ SOCKET/8_200806_total.py
@@ -38,84 +38,141 @@ tuple_cnt = 0
 time_cnt = 0
 flag_create_df = 0
 
-def create_df(item_cnt) :
-    for i in range(1, item_cnt+1) :
-        cols = []
-        for j in range(1, i+1) :
-            item = "item" + str(j)
-            cols.append(item)
 
-        cols.append("cnt")
-        cols.append("total")
-        cols.append("support")
+def check_item(item, item_cnt, temp_df) :
+    update = 0
 
-        globals()['item_{}_fp'.format(i)] = pd.DataFrame(columns = cols)
+    if item_cnt == 1 :
+        for k in range(len(temp_df)) :
+            if item == temp_df.item1[k] :
+                temp_cnt = temp_df.cnt[k]
+                new_cnt = temp_cnt + 1
+                temp_df.cnt[k] = new_cnt
+                update = 1
 
-def forloop(receive, item_cnt, temp_df, times) :
-    if times == item_cnt :
-        print("times == iten_cnt")
-        for j in range(len(temp_df)) :
+def insert(item_cnt, item) :
+    insert = []
+    for i in range(1, item_cnt) :
+        insert.append("item"+str(i))
+    insert.append(item)
+    insert.append(1)
+    insert.append('')
+    insert.append('')
+
+    print("insert : ", insert)
+    return insert
+
+def insert2(item_cnt, items) :
+    insert = []
+    for i in range(item_cnt) :
+        insert.append(items[i])
+    insert.append(1)
+    insert.append('')
+    insert.append('')
+
+    print("insert : ", insert)
+    return insert
+
+def forloop(item_cnt, start, for_cnt, receive, temp_df, items) :
+    print("for loop start 2")
+    if for_cnt == item_cnt :
+        for i in range(start, len(receive)) :
             update = 0
-            item = receive[j][2]
+            items.append(receive[i][2])
+            # globals()['item{}'.format(item_cnt)] = receive[i][2]
+            
+            for row in range(len(temp_df)) :
+                # for a in range(1, item_cnt+1) :
+                #     if globals()['item{}'.format(a)] == temp_df.iloc[row, a-1] :
+                #         update = 1
+                #     else :
+                #         update = 0
+                #         break
 
-            if item_cnt == 1 :
-                for k in range(len(temp_df)) :
-                    if item == temp_df.item2[k] :
-                        temp_cnt = temp_df.cnt[k]
-                        new_cnt = temp_cnt + 1
-                        temp_df.cnt[k] = new_cnt
+                for a in range(item_cnt) :
+                    if items[a] == temp_df.iloc[row, a] :
                         update = 1
+                    else :
+                        update = 0
+                        break
+
+                if update == 1 :        ## 동일한 item set 이 있는 경우
+                    temp_cnt = temp_df.cnt[row]
+                    new_cnt = temp_cnt + 1
+                    temp_df.cnt[row] = new_cnt
+                    break               ## temp_df를 더이상 체크하지 않고 빠져나감
             
             if update == 0 :
-                insert = []
-                for x in range(1, item_cnt) :
-                    insert.append("anchor_"+str(x))
-                insert.append(item)
-                insert.append(1)
-                insert.append('')
-                insert.append('')
+                # temp_df.loc[len(temp_df)] = insert(item_cnt, globals() ['item{}'.format(item_cnt)])      ## 신규 데이터를 insert
+                # temp_df.loc[len(temp_df)] = insert(item_cnt, items[item_cnt-1])      ## 신규 데이터를 insert
+                temp_df.loc[len(temp_df)] = insert2(item_cnt, items)      ## 신규 데이터를 insert
 
-                temp_df.loc[len(temp_df)] = insert
+            items.pop()
+        return temp_df
 
+    else :
+        print("for loop over 2")
+        for i in range(start, len(receive)) :
+            items.append(receive[i][2])
+            # globals()['item{}'.format(for_cnt)] = receive[i][2]
+            # print("item",for_cnt,':', globals()['item{}'.format(for_cnt)])
+            next_start = i + 1
+            next_for_cnt = for_cnt + 1
+            temp_df = forloop(item_cnt, next_start, next_for_cnt, receive, temp_df, items)
+            
+            items.pop()
 
-def item_test(receive, item_cnt) :
+        return temp_df
+
+# len_receive = 4
+# receive = [1]
+# forloop(len(receive), 0, 1, receive)
+ 
+# def forloop(item_cnt, receive, temp_df) :
+#     print("for loop start : ", item_cnt)
+#     # if item_cnt == 1 :
+#     for i in range(len(receive)) :
+#         update = 0
+#         globals()['item{}'.format(item_cnt)] = receive[i][2]
+        
+#         for row in range(len(temp_df)) :
+#             for a in range(1, item_cnt+1) :
+#                 if globals()['item{}'.format(a)] == temp_df.iloc[row, a-1] :
+#                     update = 1
+#                 else :
+#                     update = 0
+#                     break
+
+#             if update == 1 :        ## 동일한 item set 이 있는 경우
+#                 temp_cnt = temp_df.cnt[row]
+#                 new_cnt = temp_cnt + 1
+#                 temp_df.cnt[row] = new_cnt
+#                 break               ## temp_df를 더이상 체크하지 않고 빠져나감
+        
+#         if update == 0 :
+#             temp_df.loc[len(temp_df)] = insert(item_cnt, globals() ['item{}'.format(item_cnt)])      ## 신규 데이터를 insert
+
+#     return temp_df
+
+def item_test(item_cnt, receive) :
+    print("item test start")
     temp_df = globals()['item_{}_fp'.format(item_cnt)]
-    times = 1
+    print("item test temp_df : ", temp_df)
 
-    forloop(receive, item_cnt, temp_df, times)
+    # temp_df = forloop(item_cnt, receive, temp_df)
+    items = []
+    temp_df = forloop(item_cnt, 0, 1, receive, temp_df, items)
 
-    # for i1 in range(len(receive)) :
-    #     item = receive[i1][2]
-    #     update = 0
-
-    #     for j in range(len(temp_df)) :
-    #         if item == temp_df.item1[j] :
-    #             temp_cnt = temp_df.cnt[j]
-    #             new_cnt = temp_cnt + 1
-    #             temp_df.cnt[j] = new_cnt
-    #             update = 1
-        
-    #     if update == 0 :        ## data 신규 insert
-    #         insert = []
-    #         for x in range(1, item_cnt) :
-    #             insert.append("anchor_"+str(x))
-    #         insert.append(item)
-    #         insert.append(1)
-    #         insert.append('')
-    #         insert.append('')
-
-    #         temp_df.loc[len(temp_df)] = insert
-        
+    ## 공통부분 : count, support
     total = 0
-
     for x in range(len(temp_df)) :
         total = total + temp_df.cnt[x]
-    
     temp_df.total[0] = total
-
     for x in range(len(temp_df)) :
         support = temp_df.cnt[x] / total
         temp_df.support[x] = support
+
+    globals()['item_{}_fp'.format(item_cnt)] = temp_df
 
 def item_1_FP(receive) :
     for i in range(len(receive)) :
@@ -237,6 +294,19 @@ def item_4_FP(receive) :
                         support = item_4_fp.cnt[m] / total
                         item_4_fp.support[m] = support
 
+def create_df(item_cnt) :
+    for i in range(1, item_cnt+1) :
+        cols = []
+        for j in range(1, i+1) :
+            item = "item" + str(j)
+            cols.append(item)
+
+        cols.append("cnt")
+        cols.append("total")
+        cols.append("support")
+
+        globals()['item_{}_fp'.format(i)] = pd.DataFrame(columns = cols)
+
 while True :
     data = clientSocket.recv(65535)
     data = data.decode()        ## bytes -> string
@@ -256,23 +326,20 @@ while True :
                     create_df(item_cnt)
                     flag_create_df = 1
 
-                # item_1_FP(receive)
-                item_test(receive, 1)
-                item_2_FP(receive)
-                item_3_FP(receive)
-                item_4_FP(receive)
+                for i in range(1, item_cnt+1) :
+                    item_test(i, receive)
 
-                if agg_type == 1 and tuple_cnt == agg_unit :
-                    tuple_cnt = 0
-                    print("---  1 item FP  ---")
-                    print(item_1_fp)
 
-                    print("---  2 item FP  ---")
-                    print(item_2_fp)
-                    print("---  3 item FP  ---")
-                    print(item_3_fp)
-                    print("---  4 item FP  ---")
-                    print(item_4_fp)
+                # if agg_type == 1 and tuple_cnt == agg_unit :
+                #     tuple_cnt = 0
+                print("---  1 item FP  ---")
+                print(item_1_fp)
+                print("---  2 item FP  ---")
+                print(item_2_fp)
+                print("---  3 item FP  ---")
+                print(item_3_fp)
+                print("---  4 item FP  ---")
+                print(item_4_fp)
 
                 receive = []
                 first = 1
--- SOCKET/9_test.py
+++ SOCKET/9_test.py
@@ -1,7 +1,48 @@
-temp = 1
+# import pandas as pd
 
-def forloop(a, b) :
-    for i in range(a) :
-        
+# temp_df = pd.DataFrame(columns = ['item1', 'item2'])
 
-for i in range(temp) :
+# # # print(temp_df)
+
+# temp_df.loc[len(temp_df)] = [1, 2]
+# print(temp_df)
+
+# item_cnt = 2
+# item1 = 1
+# item2 = 3
+
+# print("temp : ", temp_df.iloc[0, 1])
+
+# for k in range(len(temp_df)) :
+#     for a in range(1, item_cnt+1) :
+#         if globals() ['item{}'.format(a)] == temp_df.iloc[k, a-1] :
+#             update = 1
+#         else :
+#             update = 0
+
+# print("update : ", update)
+
+def forloop(item_cnt, start, for_cnt, receive) :
+    if for_cnt == item_cnt :
+        for i in range(start, len(receive)) :
+            globals()['item{}'.format(for_cnt)] = receive[i]
+            print("item",for_cnt,':', globals()['item{}'.format(for_cnt)])
+
+    else :
+        for i in range(start, len(receive)) :
+            globals()['item{}'.format(for_cnt)] = receive[i]
+            print("item",for_cnt,':', globals()['item{}'.format(for_cnt)])
+            start = i + 1
+            forloop(item_cnt, start, for_cnt+1, receive)
+
+len_receive = 4
+receive = [1]
+forloop(len(receive), 0, 1, receive)
+    
+
+# for i1 in range(len(receive)) :
+#         item1 = receive[i1][2]
+#         for i2 in range(i1+1, len(receive)) :
+#             item2 = receive[i2][2]
+#             for i3 in range(i2+1, len(receive)) :
+#                 AAAAAA
